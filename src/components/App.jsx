d98ea5818c007638aa6397bb7bc4ce7b2af01b6f
Nina Pecherska <npecherska@gmail.com>
2023-12-07 00:45:25 +0200

    add Lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from 'Layout/Layout';
 import Home from 'pages/Home/Home';
 // import Movies from 'pages/Movies/Movies';
-import MovieDetails from 'pages/MovieDetails/MovieDetails';
-import Cast from 'pages/Cast/Cast';
-import Reviews from 'pages/Reviews/Reviews';
-import NotFound from './NotFound/NotFound';
+// import MovieDetails from 'pages/MovieDetails/MovieDetails';
+// import Cast from 'pages/Cast/Cast';
+// import Reviews from 'pages/Reviews/Reviews';
+// import NotFound from './NotFound/NotFound';
 import { lazy } from 'react';
 
 const Movies = lazy(() => import('pages/Movies/Movies'));
-// const MovieDetails = lazy(() => import('pages/MovieDetails'));
-// const Cast = lazy(() => import('components/MovieDetails/Cast'));
-// const Reviews = lazy(() => import('components/MovieDetails/Reviews'));
-// const NotFound = lazy(() => import('pages/NotFound'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('pages/Cast/Cast'));
+const Reviews = lazy(() => import('pages/Reviews/Reviews'));
+const NotFound = lazy(() => import('./NotFound/NotFound'));
 
 export const App = () => {
   return (
