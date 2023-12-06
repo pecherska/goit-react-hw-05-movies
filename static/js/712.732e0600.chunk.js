c121a979ef4cc5ffff1fa5b2c146a398881266eb
pecherska <97174413+pecherska@users.noreply.github.com>
2023-12-06 22:46:07 +0000

    Deploying to gh-pages from @ pecherska/goit-react-hw-05-movies@d98ea5818c007638aa6397bb7bc4ce7b2af01b6f 🚀

---
+++ static/js/712.732e0600.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[712],{712:function(n,e,t){t.r(e),t.d(e,{default:function(){return s}});var i,r=t(168),c=t(5867).ZP.p(i||(i=(0,r.Z)(["\n  font-size: 18px;\n  line-height: 24px;\n  font-weight: 500;\n  margin: 16px;\n  color: black;\n"]))),o=t(184),s=function(n){var e=n.message;return(0,o.jsx)(c,{children:e})}}}]);
+//# sourceMappingURL=712.732e0600.chunk.js.map
